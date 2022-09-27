74d8da57603d8600f0f8bdcfcd71b5fba1d30150
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-09-27 16:36:58 -0400

    Finish Forecast Accordion w/ styles.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Search from "./components/Search/Search";
 import CurrentWeather from "./components/Current-Weather/Current-Weather";
+import Forecast from "./components/Forecast/Forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import { useState } from "react";
 
@@ -35,6 +36,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
