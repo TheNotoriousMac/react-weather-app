fbc62294f761e13feeb02a11dbd83cceb70e7281
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-09-26 20:10:41 -0400

    Finished search component & start CurrentWeather.

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '688268f4d0msh69176cddbc5ed7bp1008d5jsn1f63b7d6afe9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
---
+++ src/components/Current-Weather/Current-Weather.js
@@ -0,0 +1,9 @@
+import "./Current-Weather.styles.css";
+
+const CurrentWeather = () => {
+  return (
+    <div>Current-Weather</div>
+  );
+};
+
+export default CurrentWeather;
